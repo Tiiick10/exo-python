a57792e3555177115cd0f242e0908c7c36d44d81
Tiick10 <fab.charlewood@gmail.com>
2025-04-02 09:37:20 +0200

    correction

--- Semaine1/Jour 2/ExoBonus/bonus2.py
+++ Semaine1/Jour 2/ExoBonus/bonus2.py
@@ -1,14 +1,22 @@
-n = 8
+nombre_ligne = ""
 
-for i in range(1, n + 1):
-    
-    for s in range(n - i):
-        print(" ", end="")
+while True:
+    try:
+        nombre_ligne = int(input('entrez un nombre postif'))
+    except ValueError:
+        print('valeur incorrecte !')
+        continue
+    if nombre_ligne > 0:
+        print('entrez un nombre plus grand que 2')
+        continue
+    break
 
-    for j in range(1, i + 1):
-        print(j, end="")
-
-    for j in range(i - 1, 0, -1):
-        print(j, end="")
-
+for i in range(1, nombre_ligne+1):
+    espace = nombre_ligne-i
+    for j in range(espace):
+        print(f"  ",end="")
+    for j in range(i, 2*i):
+        print(j,end=" ")
+    for j in range(2*i-2, i-1, -1):
+        print(j,end=" ")
     print()
